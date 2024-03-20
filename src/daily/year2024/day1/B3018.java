98fd5bcf10d34efe1821cc12e1ad918a1e2795eb
Nickolodeon98 <sjeon0730@gmail.com>
2024-03-20 12:55:22 +0900

    feat: 테스트 중

--- src/daily/year2024/day1/B3018.java
+++ src/daily/year2024/day1/B3018.java
@@ -1,4 +1,49 @@
 package daily.year2024.day1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class B3018 {
+
+    final static int SUN_YOUNG = 1;
+    static int[] perceived;
+    public static int findGenius(int total, List<Integer> members) {
+        for (Integer member : members) {
+            perceived[member-1] = 1;
+        }
+
+        System.out.println(Arrays.toString(perceived));
+
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        List<Integer> genius = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        perceived = new int[N];
+
+        int E = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < E; i++) {
+            List<Integer> participants = new ArrayList<>();
+            String[] infos = br.readLine().split(" ");
+            int K = Integer.parseInt(infos[0]);
+            for (int j = 1; j <= K; j++) {
+                participants.add(Integer.parseInt(infos[j]));
+            }
+            genius.add(findGenius(K, participants));
+        }
+
+        for (Integer g : genius) {
+            System.out.println(g);
+        }
+    }
 }
