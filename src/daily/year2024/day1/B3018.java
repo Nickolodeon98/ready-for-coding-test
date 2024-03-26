ed2b760d21161a60e3d5c32db910a27e52b35b65
Nickolodeon98 <sjeon0730@gmail.com>
2024-03-26 13:01:58 +0900

    feat: findGenius 메서드 리턴값 1에서 배열로 변경

--- src/daily/year2024/day1/B3018.java
+++ src/daily/year2024/day1/B3018.java
@@ -3,28 +3,24 @@ package daily.year2024.day1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class B3018 {
 
     final static int SUN_YOUNG = 1;
     static int[] perceived;
-    public static int findGenius(int total, List<Integer> members) {
+    public static int[] findGenius(int total, List<Integer> members) {
         for (Integer member : members) {
             perceived[member-1] = 1;
         }
 
-//        System.out.println(Arrays.toString(perceived));
-
-        return 1; // returns anything that is of type 'integer'
+        return perceived; // returns anything that is of type 'integer'
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Integer> genius = new ArrayList<>();
+        Map<String, int[]> genius = new HashMap<>();
         int N = Integer.parseInt(br.readLine());
 
 
@@ -38,8 +34,7 @@ public class B3018 {
             for (int j = 1; j <= K; j++) {
                 participants.add(Integer.parseInt(infos[j]));
             }
-            genius.add(findGenius(K, participants));
-
+            genius.put(String.valueOf(i), findGenius(K, participants));
         }
 
         System.out.println(genius);
